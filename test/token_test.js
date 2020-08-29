d56cae96937127fa660aacf7d8d7e7a4594994e4
Cybergen <41336298+Cybergen300@users.noreply.github.com>
2020-08-29 20:07:20 +0200

    Update token_test.js

--- test/token_test.js
+++ test/token_test.js
@@ -2,7 +2,6 @@
 
 //We call our token contract
 const Token = artifacts.require('./token.sol')
-const Buy = artifacts.require('./buy.sol')
 
 //load the chai library enabling us to use its TDD assert style
 require('chai')
@@ -18,18 +17,12 @@ function decimals(n) {
 //We deploy our token contract
 contract('token contract testing', ([deployer, investor]) => {
 	//varibale declaration
-	let token, buy
+	let token
 
 	before(async() => {
 
 		//Create a new instance of our token contract
 		token = await Token.new()
-
-		//Create a new instance of our buy contract
-		buy = await Buy.new(token.address)
-
-		//transfer all the tokens to the buy contract 
-		await token.transfer(buy.address, decimals('1000000'))
 
 	})
 
@@ -44,31 +37,6 @@ contract('token contract testing', ([deployer, investor]) => {
 			//total supply verification
 			const tokenSupply = await token.totalSupply()
 			//console.log(tokenSupply)
-		})
-	})
-
-	describe ('buy contract', async() => {
-		it('buy contract specs verification', async() => {
-			//name verification
-			const buyName = await buy.name()
-			assert.equal(buyName, "Buy token contract")
-
-			//verify that the tokens migration has indeed happened
-			const buyBalance = await token.totalSupply()
-			assert.equal(buyBalance, 1000000000000000000000000)
-		})
-	})
-
-	describe('test of the buy function for our token', async() => {
-		before(async() => {
-			//Perform the dummy buy of 10 tokens
-			result = await buy.buysETH({from: investor, value: decimals('10')})
-		})
-
-		it('Allows user to buy tokens from the buy contract for a fixed price', async() => {
-			//Check investor token balance after purchase to see if it's indeed equal to 10 
-			let investorBalance = await token.balanceOf(investor)
-			assert.equal(investorBalance, decimals('10'))
 		})
 	})
 })
